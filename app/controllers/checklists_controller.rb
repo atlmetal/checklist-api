60393748154d3496562c0d18da735833458a0a91
atlmetal <mateodlr90@gmail.com>
2023-04-23 11:59:29 -0500

    added checklist crud

--- app/controllers/checklists_controller.rb
+++ app/controllers/checklists_controller.rb
@@ -1,6 +1,41 @@
 class ChecklistsController < ApplicationController
+  before_action :set_checklist, only: [:show, :update, :destroy]
+
   def index
     @checklists = Checklist.all
     json_response(@checklists)
+  end
+
+  #POST /checklists
+  def create
+    @checklists = Checklist.create!(checklist_params)
+    json_response(@checklists, :created)
+  end
+
+  #GET /checklists/:id
+  def show
+    json_response(@checklists)
+  end
+
+  # PUT /checklists/:id
+  def update
+    @checklists.update(checklist_params)
+    head :no_content
+  end
+
+  def destroy
+    @checklists.destroy
+    head :no_content
+  end
+
+  private
+
+  def checklist_params
+    # whitelist params
+    params.permit(:tittle, :created_by)
+  end
+
+  def set_checklist
+    @checklists = Checklist.find(params[:id])
   end
 end
