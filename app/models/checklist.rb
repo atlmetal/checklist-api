92133deff1451d30788baca4ff8e84bc2c253e7d
atlmetal <mateodlr90@gmail.com>
2023-04-23 10:50:40 -0500

    Added model validations and migratios

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -1,2 +1,5 @@
 class Checklist < ApplicationRecord
+  has_many :tasks, dependent: :destroy
+
+  validate_presence_of :tittle, :created_by
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :checklist
+
+  validate_presence_of :name
 end
